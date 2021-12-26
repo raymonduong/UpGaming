cddb14fc5790d5313b5cc66dc906ed73a3c03913
Raymond <raymonduong@gmail.com>
2021-12-26 14:27:42 -0700

    Added images

--- src/App.js
+++ src/App.js
@@ -2,20 +2,52 @@ import logo from "./logo.svg";
 import "./App.css";
 import "atropos/css";
 import Atropos from "atropos/react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Helmet } from "react-helmet";
 <link rel="stylesheet" href="path/to/atropos.css" />;
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-  crossorigin="anonymous"
-/>;
 
 export default function App() {
   return (
-    <div id="app">
-      {/* Atropos */}
-      <Atropos className="my-atropos">{/* ... */}</Atropos>
+    <div id="app" className="App">
+      <Helmet>
+        <title>Games of 2022</title>
+      </Helmet>
+      <Atropos className="atropos">
+        <img
+          src={"assets/Cover Art/SIFU.png"}
+          className="game-box game-box-bg"
+          data-atropos-offset="-4"
+        ></img>
+        <img
+          src={"assets/Logo/SIFU.png"}
+          className="game-box-logo game-box"
+          data-atropos-offset="5"
+        ></img>
+      </Atropos>
+      <Atropos>
+        <img
+          src={"assets/Cover Art/HFW.png"}
+          className="game-box game-box-bg"
+          data-atropos-offset="-4"
+        ></img>
+        <img
+          src={"assets/Logo/HFW.png"}
+          className="game-box-logo game-box"
+          data-atropos-offset="2.5"
+        ></img>
+      </Atropos>
+      <Atropos>
+        <img
+          src={"assets/Cover Art/ELDENRING.png"}
+          className="game-box game-box-bg"
+          data-atropos-offset="-4"
+        ></img>
+        <img
+          src={"assets/Logo/ELDENRING.png"}
+          className="game-box-logo game-box"
+          data-atropos-offset="2.5"
+          style={{top:"-15vh"}}
+        ></img>
+      </Atropos>
     </div>
   );
 }
