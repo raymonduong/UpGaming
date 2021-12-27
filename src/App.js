84259e19e53ad8c11ed5ec32bb102b30877b0894
Raymond Duong <raymonduong@gmail.com>
2021-12-27 05:40:35 -0700

    Added studio logos

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ export default function App() {
         <title>Games of 2022</title>
       </Helmet>
       <p className="black">Ayy its a me mario</p>
-      <div className="flex flex-col gap-4 md:flex-row">
+      <div className="flex flex-col gap-4 md:flex-row md:flex-wrap">
         <Atropos className="atropos">
           <img
             src={"assets/Cover Art/SIFU.png"}
-            className="game-box game-box-bg rounded-xl"
+            className="game-box game-box-bg"
             data-atropos-offset="-4"
           ></img>
           <img
@@ -24,30 +24,48 @@ export default function App() {
             className="game-box-logo game-box"
             data-atropos-offset="5"
           ></img>
+          <img
+            src={"assets/Studios/Sloclap.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{width:"100px",bottom:"-15px",right:"0"}}
+          ></img>
         </Atropos>
         <Atropos>
           <img
             src={"assets/Cover Art/HFW.png"}
-            className="game-box game-box-bg rounded-xl"
+            className="game-box game-box-bg"
             data-atropos-offset="-4"
           ></img>
           <img
             src={"assets/Logo/HFW.png"}
             className="game-box-logo game-box"
-            data-atropos-offset="2.5"
+            data-atropos-offset="5"
+          ></img>
+          <img
+            src={"assets/Studios/ps.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{width:"30px", bottom:"-15px"}}
           ></img>
         </Atropos>
         <Atropos>
           <img
             src={"assets/Cover Art/ELDENRING.png"}
-            className="game-box game-box-bg rounded-xl"
+            className="game-box game-box-bg"
             data-atropos-offset="-4"
           ></img>
           <img
             src={"assets/Logo/ELDENRING.png"}
             className="game-box-logo game-box"
-            data-atropos-offset="2.5"
+            data-atropos-offset="5"
             style={{ top: "-15vh" }}
+          ></img>
+          <img
+            src={"assets/Studios/BandaiNamco.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{width:"auto", bottom:"-20px"}}
           ></img>
         </Atropos>
       </div>
