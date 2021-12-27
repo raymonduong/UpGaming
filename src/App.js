07ca0abc6ea2eb5bca4b514df4cc70c41b2e3e81
Raymond Duong <raymonduong@gmail.com>
2021-12-27 12:42:10 -0700

    Compressed Images and added more

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ export default function App() {
   return (
     <div id="app" className="App">
       <Helmet>
-        <title>Up and Gaming</title>
+        <title>UpGaming</title>
       </Helmet>
-      <h1 className="text-center text-4xl md:text-9xl mt-52 mb-8">
-        Up and Gaming
-      </h1>
+      <h1 className="text-center text-4xl md:text-9xl mt-52 mb-8">UpGaming</h1>
       <h2 className="text-center text-4xl md:text-3xl mb-56">
-        Have a look at up-and-coming games
+        Have a look at these upcoming video games
       </h2>
-      <div className="flex flex-col gap-5 mb-10 md:flex-row md:flex-wrap content-center justify-center ml-auto mr-auto w-2/3">
+      <div className="flex flex-col gap-5 mb-10 md:flex-row md:flex-wrap content-center justify-center ml-auto mr-auto w-3/4">
         <Atropos className="atropos">
           <img
             src={"assets/Cover Art/SIFU.png"}
@@ -231,6 +229,92 @@ export default function App() {
             data-atropos-offset="3"
             style={{ width: "30px", bottom: "-15px" }}
             alt="PlayStation Studios Logo"
+          ></img>
+        </Atropos>
+        <Atropos className="atropos">
+          <img
+            src={"assets/Cover Art/Forspoken.png"}
+            className="game-box game-box-bg rounded-md"
+            data-atropos-offset="-4"
+            alt="Forspoken Cover Art"
+          ></img>
+          <img
+            src={"assets/Logo/Forspoken.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="5"
+            alt="Forspoken Logo"
+          ></img>
+          <img
+            src={"assets/Studios/SQ.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{ width: "100px", bottom: "-15px", right: "0" }}
+            alt="Square Enix Logo"
+          ></img>
+        </Atropos>
+        <Atropos className="atropos">
+          <img
+            src={"assets/Cover Art/Stalker2.png"}
+            className="game-box game-box-bg rounded-md"
+            data-atropos-offset="-4"
+            alt="Stalker 2 Art"
+          ></img>
+          <img
+            src={"assets/Logo/Stalker2.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="5"
+            alt="Stalker 2 Logo"
+          ></img>
+          <img
+            src={"assets/Studios/GameWorld.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{ width: "auto", bottom: "-20px", right: "10px" }}
+            alt="Game World Logo"
+          ></img>
+        </Atropos>
+        <Atropos>
+          <img
+            src={"assets/Cover Art/Kirby.png"}
+            className="game-box game-box-bg rounded-md"
+            data-atropos-offset="-4"
+            alt="Kirby and the Forgetten Land Cover Art"
+          ></img>
+          <img
+            src={"assets/Studios/Nintendo.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{ width: "45px", top: "-18px", left: "0" }}
+            alt="Nintendo Switch Logo"
+          ></img>
+          <img
+            src={"assets/Logo/Kirby.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="5"
+            style={{ top: "-90px" }}
+            alt="Kirby and the Forgetten Land Logo"
+          ></img>
+        </Atropos>
+        <Atropos className="atropos">
+          <img
+            src={"assets/Cover Art/Redfall.png"}
+            className="game-box game-box-bg rounded-md"
+            data-atropos-offset="-4"
+            alt="Redfall Art"
+          ></img>
+          <img
+            src={"assets/Logo/Redfall.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="5"
+            style={{top:"-130px", left:"15px"}}
+            alt="Redfall Logo"
+          ></img>
+          <img
+            src={"assets/Studios/Bethesda.png"}
+            className="game-box-studio game-box"
+            data-atropos-offset="3"
+            style={{ width: "80px", bottom: "-20px", right: "10px" }}
+            alt="Bethesda Logo"
           ></img>
         </Atropos>
       </div>
