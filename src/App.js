dcf2b168955688a269673274295d2db69952bc3c
Raymond Duong <raymonduong@gmail.com>
2021-12-26 18:57:06 -0700

    Added tailwind css, background color, and etc.

--- src/App.js
+++ src/App.js
@@ -11,43 +11,46 @@ export default function App() {
       <Helmet>
         <title>Games of 2022</title>
       </Helmet>
-      <Atropos className="atropos">
-        <img
-          src={"assets/Cover Art/SIFU.png"}
-          className="game-box game-box-bg"
-          data-atropos-offset="-4"
-        ></img>
-        <img
-          src={"assets/Logo/SIFU.png"}
-          className="game-box-logo game-box"
-          data-atropos-offset="5"
-        ></img>
-      </Atropos>
-      <Atropos>
-        <img
-          src={"assets/Cover Art/HFW.png"}
-          className="game-box game-box-bg"
-          data-atropos-offset="-4"
-        ></img>
-        <img
-          src={"assets/Logo/HFW.png"}
-          className="game-box-logo game-box"
-          data-atropos-offset="2.5"
-        ></img>
-      </Atropos>
-      <Atropos>
-        <img
-          src={"assets/Cover Art/ELDENRING.png"}
-          className="game-box game-box-bg"
-          data-atropos-offset="-4"
-        ></img>
-        <img
-          src={"assets/Logo/ELDENRING.png"}
-          className="game-box-logo game-box"
-          data-atropos-offset="2.5"
-          style={{top:"-15vh"}}
-        ></img>
-      </Atropos>
+      <p className="black">Ayy its a me mario</p>
+      <div className="flex flex-col gap-4 md:flex-row">
+        <Atropos className="atropos">
+          <img
+            src={"assets/Cover Art/SIFU.png"}
+            className="game-box game-box-bg rounded-xl"
+            data-atropos-offset="-4"
+          ></img>
+          <img
+            src={"assets/Logo/SIFU.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="5"
+          ></img>
+        </Atropos>
+        <Atropos>
+          <img
+            src={"assets/Cover Art/HFW.png"}
+            className="game-box game-box-bg rounded-xl"
+            data-atropos-offset="-4"
+          ></img>
+          <img
+            src={"assets/Logo/HFW.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="2.5"
+          ></img>
+        </Atropos>
+        <Atropos>
+          <img
+            src={"assets/Cover Art/ELDENRING.png"}
+            className="game-box game-box-bg rounded-xl"
+            data-atropos-offset="-4"
+          ></img>
+          <img
+            src={"assets/Logo/ELDENRING.png"}
+            className="game-box-logo game-box"
+            data-atropos-offset="2.5"
+            style={{ top: "-15vh" }}
+          ></img>
+        </Atropos>
+      </div>
     </div>
   );
 }
