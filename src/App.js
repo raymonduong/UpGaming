b3be33a5e29c9e689a77c4db6722d3cf86b02a38
Raymond <raymonduong@gmail.com>
2022-01-06 09:02:21 -0700

    fixed small bug

--- src/App.js
+++ src/App.js
@@ -4296,7 +4296,7 @@ export default class App extends Component {
                 className="mb-10"
               ></YoutubeEmbed>
               <button
-                onClick={this.handleCloseModa50}
+                onClick={this.handleCloseModal50}
                 className="absolute inline-flex items-center justify-center p-2 text-gray-100 rounded-md bg-slate-800 top-5 right-5 hover:text-gray-200 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
               >
                 <span class="sr-only">Close menu</span>{" "}
